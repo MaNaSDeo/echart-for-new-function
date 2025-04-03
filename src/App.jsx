8e7951c2d02e6cef311d4eaa908d9587543b23be
MaNaSDeo <rmnslogin@gmail.com>
2025-04-03 23:23:04 +0530

    basic function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,54 @@
-import "./App.css";
+import ReactECharts from "echarts-for-react";
+import {
+  // modifiedData as data,
+  testModifiedData,
+  transformData,
+} from "./helperFunctions";
+import { originalData } from "./fakeData";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div>Manas deo</div>;
+  console.log("testModifiedData", { testModifiedData });
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    setData(transformData(originalData));
+  }, []);
+
+  const option = {
+    title: { text: "Fuel Level Over Time" },
+    tooltip: { trigger: "axis" },
+    toolbox: {
+      show: false,
+    },
+    xAxis: {
+      type: "time",
+      // boundaryGap: ["5%", "5%"], // Fix applied here
+      boundaryGap: false,
+      min: "dataMin", // Start x-axis from the first data point
+      max: "dataMax", // End x-axis at the last data point
+    },
+    yAxis: {
+      type: "value",
+      axisLabel: {
+        formatter: "{value} L",
+      },
+      axisPointer: {
+        snap: false,
+      },
+    },
+    series: data,
+  };
+  return (
+    <div>
+      <ReactECharts
+        option={option}
+        style={{ height: "100vh", width: "100vw" }}
+        notMerge={true}
+        lazyUpdate={true}
+        key={JSON.stringify(option)} // Force update when option changes
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/helperFunctions.js
@@ -0,0 +1,529 @@
+export const data = [
+  {
+    device_timestamp: 1735257741000,
+    speed: 0,
+    ignition: false,
+    level: 234.75,
+  },
+  {
+    device_timestamp: 1735258041000,
+    speed: 0,
+    ignition: false,
+    level: 189.75,
+  },
+  {
+    device_timestamp: 1735258341000,
+    speed: 0,
+    ignition: false,
+    level: 186.75,
+  },
+  {
+    device_timestamp: 1735258641000,
+    speed: 0,
+    ignition: false,
+    level: 231.75,
+  },
+  {
+    device_timestamp: 1735258941000,
+    speed: 0,
+    ignition: false,
+    level: 289.13,
+  },
+  {
+    device_timestamp: 1735259241000,
+    speed: 0,
+    ignition: true,
+    level: 244.75,
+  },
+  {
+    device_timestamp: 1735259541000,
+    speed: 40,
+    ignition: true,
+    level: 237.25,
+  },
+  {
+    device_timestamp: 1735259841000,
+    speed: 37,
+    ignition: true,
+    level: 229.75,
+  },
+  {
+    device_timestamp: 1735260001000,
+    speed: 48,
+    ignition: true,
+    level: 225.75,
+  },
+  {
+    device_timestamp: 1735261201000,
+    speed: 0,
+    ignition: true,
+    level: 350,
+  },
+  {
+    device_timestamp: 1735261221000,
+    speed: 0,
+    ignition: false,
+    level: 310.87,
+  },
+  {
+    device_timestamp: 1735261241000,
+    speed: 0,
+    ignition: false,
+    level: 271.74,
+  },
+  {
+    device_timestamp: 1735261261000,
+    speed: 0,
+    ignition: false,
+    level: 249,
+  },
+  {
+    device_timestamp: 1735261281000,
+    speed: 0,
+    ignition: false,
+    level: 246.75,
+  },
+  {
+    device_timestamp: 1735261301000,
+    speed: 0,
+    ignition: false,
+    level: 244.5,
+  },
+  {
+    device_timestamp: 1735261321000,
+    speed: 0,
+    ignition: true,
+    level: 242.25,
+  },
+  {
+    device_timestamp: 1735261341000,
+    speed: 0,
+    ignition: true,
+    level: 240,
+  },
+];
+export const modifiedData = [
+  {
+    type: "line",
+    lineStyle: {
+      type: "solid",
+      color: "yellow",
+    },
+    symbol: "none",
+    areaStyle: {
+      color: {
+        colorStops: [
+          {
+            offset: 0,
+            color: "#ACD4FD",
+          },
+          {
+            offset: 1,
+            color: "#F5FAFF",
+          },
+        ],
+        x: 0,
+        y: 0,
+        x2: 0,
+        y2: 1,
+        type: "linear",
+        global: false,
+      },
+    },
+    data: [
+      [
+        1735257741000,
+        234.75,
+        {
+          device_timestamp: 1735257741000,
+          speed: 0,
+          ignition: false,
+          level: 234.75,
+        },
+      ],
+      [
+        1735258041000,
+        189.75,
+        {
+          device_timestamp: 1735258041000,
+          speed: 0,
+          ignition: false,
+          level: 189.75,
+        },
+      ],
+      [
+        1735258341000,
+        186.75,
+        {
+          device_timestamp: 1735258341000,
+          speed: 0,
+          ignition: false,
+          level: 186.75,
+        },
+      ],
+      [
+        1735258641000,
+        231.75,
+        {
+          device_timestamp: 1735258641000,
+          speed: 0,
+          ignition: false,
+          level: 231.75,
+        },
+      ],
+      [
+        1735258941000,
+        289.13,
+        {
+          device_timestamp: 1735258941000,
+          speed: 0,
+          ignition: false,
+          level: 289.13,
+        },
+      ],
+    ],
+  },
+  {
+    type: "line",
+    lineStyle: {
+      type: "solid",
+      color: "blue",
+    },
+    symbol: "none",
+    areaStyle: {
+      color: {
+        colorStops: [
+          {
+            offset: 0,
+            color: "#ACD4FD",
+          },
+          {
+            offset: 1,
+            color: "#F5FAFF",
+          },
+        ],
+        x: 0,
+        y: 0,
+        x2: 0,
+        y2: 1,
+        type: "linear",
+        global: false,
+      },
+    },
+    data: [
+      [
+        1735258941000,
+        289.13,
+        {
+          device_timestamp: 1735258941001,
+          speed: 0,
+          ignition: false,
+          level: 289.13,
+        },
+      ],
+      [
+        1735259241000,
+        244.75,
+        {
+          device_timestamp: 1735259241000,
+          speed: 0,
+          ignition: true,
+          level: 244.75,
+        },
+      ],
+      [
+        1735259541000,
+        237.25,
+        {
+          device_timestamp: 1735259541000,
+          speed: 40,
+          ignition: true,
+          level: 237.25,
+        },
+      ],
+      [
+        1735259841000,
+        229.75,
+        {
+          device_timestamp: 1735259841000,
+          speed: 37,
+          ignition: true,
+          level: 229.75,
+        },
+      ],
+      [
+        1735260001000,
+        225.75,
+        {
+          device_timestamp: 1735260001000,
+          speed: 48,
+          ignition: true,
+          level: 225.75,
+        },
+      ],
+      [
+        1735261201000,
+        350,
+        {
+          device_timestamp: 1735261201000,
+          speed: 0,
+          ignition: true,
+          level: 350,
+        },
+      ],
+    ],
+  },
+  {
+    type: "line",
+    lineStyle: {
+      type: "solid",
+      color: "yellow",
+    },
+    symbol: "none",
+    areaStyle: {
+      color: {
+        colorStops: [
+          {
+            offset: 0,
+            color: "#ACD4FD",
+          },
+          {
+            offset: 1,
+            color: "#F5FAFF",
+          },
+        ],
+        x: 0,
+        y: 0,
+        x2: 0,
+        y2: 1,
+        type: "linear",
+        global: false,
+      },
+    },
+    data: [
+      [
+        1735261201000,
+        350,
+        {
+          device_timestamp: 1735261201001,
+          speed: 0,
+          ignition: true,
+          level: 350,
+        },
+      ],
+      [
+        1735261221000,
+        310.87,
+        {
+          device_timestamp: 1735261221000,
+          speed: 0,
+          ignition: false,
+          level: 310.87,
+        },
+      ],
+      [
+        1735261241000,
+        271.74,
+        {
+          device_timestamp: 1735261241000,
+          speed: 0,
+          ignition: false,
+          level: 271.74,
+        },
+      ],
+      [
+        1735261261000,
+        249,
+        {
+          device_timestamp: 1735261261000,
+          speed: 0,
+          ignition: false,
+          level: 249,
+        },
+      ],
+      [
+        1735261281000,
+        246.75,
+        {
+          device_timestamp: 1735261281000,
+          speed: 0,
+          ignition: false,
+          level: 246.75,
+        },
+      ],
+      [
+        1735261301000,
+        244.5,
+        {
+          device_timestamp: 1735261301000,
+          speed: 0,
+          ignition: false,
+          level: 244.5,
+        },
+      ],
+    ],
+  },
+  {
+    type: "line",
+    lineStyle: {
+      type: "solid",
+      color: "blue",
+    },
+    symbol: "none",
+    areaStyle: {
+      color: {
+        colorStops: [
+          {
+            offset: 0,
+            color: "#ACD4FD",
+          },
+          {
+            offset: 1,
+            color: "#F5FAFF",
+          },
+        ],
+        x: 0,
+        y: 0,
+        x2: 0,
+        y2: 1,
+        type: "linear",
+        global: false,
+      },
+    },
+    data: [
+      [
+        1735261301000,
+        244.5,
+        {
+          device_timestamp: 1735261301001,
+          speed: 0,
+          ignition: false,
+          level: 244.5,
+        },
+      ],
+      [
+        1735261321000,
+        242.25,
+        {
+          device_timestamp: 1735261321000,
+          speed: 0,
+          ignition: true,
+          level: 242.25,
+        },
+      ],
+      [
+        1735261341000,
+        240,
+        {
+          device_timestamp: 1735261341000,
+          speed: 0,
+          ignition: true,
+          level: 240,
+        },
+      ],
+    ],
+  },
+];
+
+/**
+ * Transforms the original data into a modified format based on ignition value changes
+ * @param {Array} originalData - The original data array
+ * @returns {Array} The transformed data in the format needed
+ */
+export function transformData(originalData) {
+  const result = [];
+  let currentSeries = [];
+  let currentIgnitionState = originalData[0].ignition;
+  let seriesColor = currentIgnitionState ? "blue" : "yellow";
+
+  // Create the first data point
+  currentSeries.push([
+    originalData[0].device_timestamp,
+    originalData[0].level,
+    { ...originalData[0] },
+  ]);
+
+  // Process the rest of the data
+  for (let i = 1; i < originalData.length; i++) {
+    const currentData = originalData[i];
+
+    // Check if ignition state has changed
+    if (currentData.ignition !== currentIgnitionState) {
+      // Finalize the current series
+      result.push(createSeriesObject(currentSeries, seriesColor));
+
+      // Start a new series with a copy of the last point but with timestamp+1
+      const lastPoint = currentSeries[currentSeries.length - 1];
+      const continuityPoint = [
+        lastPoint[0],
+        lastPoint[1],
+        {
+          ...lastPoint[2],
+          device_timestamp: lastPoint[2].device_timestamp + 1,
+        },
+      ];
+
+      // Update current ignition state and color
+      currentIgnitionState = currentData.ignition;
+      seriesColor = currentIgnitionState ? "blue" : "yellow";
+
+      // Start new series with the continuity point
+      currentSeries = [continuityPoint];
+    }
+
+    // Add current data point to the current series
+    currentSeries.push([
+      currentData.device_timestamp,
+      currentData.level,
+      { ...currentData },
+    ]);
+  }
+
+  // Add the last series if not empty
+  if (currentSeries.length > 0) {
+    result.push(createSeriesObject(currentSeries, seriesColor));
+  }
+
+  return result;
+}
+
+/**
+ * Creates a series object with the specified data points and color
+ * @param {Array} dataPoints - The data points for this series
+ * @param {string} color - The color for this series
+ * @returns {Object} The series object
+ */
+function createSeriesObject(dataPoints, color) {
+  return {
+    type: "line",
+    lineStyle: {
+      type: "solid",
+      color: color,
+    },
+    symbol: "none",
+    areaStyle: {
+      color: {
+        colorStops: [
+          {
+            offset: 0,
+            color: "#ACD4FD",
+          },
+          {
+            offset: 1,
+            color: "#F5FAFF",
+          },
+        ],
+        x: 0,
+        y: 0,
+        x2: 0,
+        y2: 1,
+        type: "linear",
+        global: false,
+      },
+    },
+    data: dataPoints,
+  };
+}
+
+// Example usage:
+export const testModifiedData = transformData(data);
